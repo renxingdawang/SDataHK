42a17ed340b1d5507e8eb5e7403d6397b3374a23
Galaxy <13087482+GalaxyPw@user.noreply.gitee.com>
2024-12-30 08:59:02 +0800

    Add idl police and use hz

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/renxingdawang/SDataHK
-
-go 1.23.3
---
+++ server/idl/police.thrift
@@ -0,0 +1,21 @@
+namespace go police_service
+
+struct Location {
+  1: double latitude;
+  2: double longitude;
+}
+
+struct PoliceStation {
+  1: string name;
+  2: Location location;
+  3: double distance; // 距离单位：米
+}
+
+struct NearestPoliceStationsResponse {
+  1: string status;
+  2: list<PoliceStation> data;
+}
+
+service PoliceService {
+  NearestPoliceStationsResponse getNearestPoliceStations(1: Location userLocation);
+}
